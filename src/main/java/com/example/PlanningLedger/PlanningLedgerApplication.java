9eee6054e55aa00e979706c1e9e4f17998a3aa99
Nikita <nikkarth@iu.edu>
2026-04-26 03:43:53 -0400

    first commit

---
+++ src/main/java/com/example/PlanningLedger/PlanningLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlanningLedger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlanningLedger/PlanningLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlanningLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
